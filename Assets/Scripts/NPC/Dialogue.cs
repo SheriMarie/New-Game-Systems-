40e4a1a787781dfd04308d95026e97bac27746fe
SheriMarie <140038384+SheriMarie@users.noreply.github.com>
2023-08-17 13:05:15 +1000

    New game systems

---
+++ Assets/Scripts/NPC/Dialogue.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public bool showDialogue = false;
+    public string speakersName = "";
+    public string[] dialogueText;
+    public int currentLineOfText = 0;
+
+    //Method that we can access when called upon in other scripts 
+    public void OpenDialouge()
+    {
+        //trigger bool
+
+        showDialogue = true;
+
+        //reset int
+        currentLineOfText = 0;
+
+        //any other thing you need 
+
+    }
+
+    public void CloseDialouge()
+    {
+        //trigger bool
+        //reset int
+        //any other thing you need
+
+    }
+}
